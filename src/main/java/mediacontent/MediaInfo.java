dc0386470f7df616197b18d7047c2a74fd933190
Leonardo Sarra <leonardosarra@outlook.com>
2017-10-10 23:20:33 +0200

    MediaInfo class is now abstract

--- src/main/java/mediacontent/MediaInfo.java
+++ src/main/java/mediacontent/MediaInfo.java
@@ -1,13 +1,7 @@
 package mediacontent;
 
-public class MediaInfo {
+public abstract class MediaInfo {
     public String title;
-    public String ISBN;
-    public String author;
-    public String summary;
-    public String publisher;
-    public String releaseDate;
-    public String genre;
 
 
 
@@ -15,53 +9,8 @@ public class MediaInfo {
         return title;
     }
 
-    public String getISBN() {
-        return ISBN;
-    }
-    public String getAuthor() {
-        return author;
-    }
-    public String getSummary() {
-        return summary;
-    }
-
-
     public void setTitle(String title) {
         this.title = title;
     }
 
-    public void setISBN(String iSBN) {
-        this.ISBN = iSBN;
-    }
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-    public void setSummary(String summary) {
-        this.summary = summary;
-    }
-
-    public void setPublisher(String publisher) {
-        this.publisher = publisher;
-    }
-
-    @Override
-    public String toString() {
-        return "Media{" +
-                "title='" + title + '\'' +
-                ", iSBN='" + ISBN + '\'' +
-                ", author='" + author + '\'' +
-                ", summary='" + summary + '\'' +
-                ", publisher='" + publisher + '\'' +
-                ", releaseDate='" + releaseDate + '\'' +
-                '}';
-    }
-
-    public void setReleaseDate(String releaseDate) {
-        this.releaseDate = releaseDate;
-    }
-
-
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
 }
