1a67012d8e78fef61c352e4f601d2fa2ae6bf8b0
Leonardo Sarra <leonardosarra@outlook.com>
2017-10-13 21:27:57 +0200

    Added Dropbox and GDrive methods

---
+++ src/main/java/framework/DbxAPIOp.java
@@ -0,0 +1,51 @@
+package framework;
+
+import com.dropbox.core.DbxException;
+import com.dropbox.core.DbxRequestConfig;
+import com.dropbox.core.v2.DbxClientV2;
+import com.dropbox.core.v2.files.FileMetadata;
+import com.dropbox.core.v2.files.ListFolderResult;
+import com.dropbox.core.v2.files.Metadata;
+import mediacontent.Greeting;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class DbxAPIOp {
+
+    public static List<String> dropboxGetFiles(String code) {
+
+        DbxRequestConfig config = new DbxRequestConfig("Media Information Service Configuration");
+        DbxClientV2 client = new DbxClientV2(config, code);
+        ListFolderResult result = null;
+        List<String> elements = new LinkedList<String>();
+
+
+        try {
+            result = client.files().listFolderBuilder("/Università").withRecursive(true).start();
+            while (true) {
+                for (Metadata metadata : result.getEntries()) {
+                    if (metadata instanceof FileMetadata) {
+                        elements.add(metadata.getName());
+                    }
+                }
+
+                if (!result.getHasMore()) {
+                    break;
+                }
+
+                result = client.files().listFolderContinue(result.getCursor());
+            }
+
+            System.out.println(elements.toString());
+        } catch (DbxException e) {
+            e.printStackTrace();
+        }
+
+
+        return elements;
+
+
+    }
+}
