96dd4fda9c8ed20bc082bd6f513e71146b49dd43
Leonardo Sarra <leonardosarra@outlook.com>
2017-10-13 22:23:00 +0200

    Updated API storage class

--- src/main/java/framework/MyAPIKey.java
+++ src/main/java/framework/MyAPIKey.java
@@ -7,11 +7,15 @@ import java.io.InputStream;
 import java.util.Properties;
 
 public class MyAPIKey {
-    private static String googlebook_api;
     private static String discogs_api;
     private static String themoviedb_api;
     private static String igdb_api;
-
+    private static String drive_id;
+    private static String drive_secret;
+    private static String dropbox_secret;
+    private static String dropbox_id;
+    private static String google_api;
+    private static String googlebook_api;
     public MyAPIKey(String file){
         Properties prop = new Properties();
         FileInputStream input = null;
@@ -22,6 +26,11 @@ public class MyAPIKey {
             themoviedb_api=prop.getProperty("moviedb");
             discogs_api=prop.getProperty("discogs");
             igdb_api=prop.getProperty("igdb");
+            drive_id=prop.getProperty("drive_id");
+            drive_secret=prop.getProperty("drive_secret");
+            dropbox_id=prop.getProperty("dropbox_id");
+            dropbox_secret=prop.getProperty("dropbox_secret");
+            google_api=prop.getProperty("google_api");
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -32,8 +41,32 @@ public class MyAPIKey {
 
     }
 
+    public static String getDropbox_secret() {
+        return dropbox_secret;
+    }
+
+    public static String getDropbox_id() {
+        return dropbox_id;
+    }
+
     public static String getGooglebook_api() {
         return googlebook_api;
+    }
+
+    public static String getIgdb_api() {
+        return igdb_api;
+    }
+
+    public static String getDrive_id() {
+        return drive_id;
+    }
+
+    public static String getDrive_secret() {
+        return drive_secret;
+    }
+
+    public static String getGoogle_api() {
+        return google_api;
     }
 
     public static String getDiscogs_api() {
