6c262c12bcfde72e3612a3a86323420d7d91a001
Leonardo Sarra <leonardosarra@outlook.com>
2017-11-11 16:56:19 +0100

    Added cookie bar to index.html

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -88,3 +88,17 @@ function openChatWindow() {
     //window.open("insert url here","",stile);
     window.location.href = "/webchat";
 }
+
+window.addEventListener("load", function(){
+    window.cookieconsent.initialise({
+        "palette": {
+            "popup": {
+                "background": "#252e39"
+            },
+            "button": {
+                "background": "#14a7d0"
+            }
+        },
+        "theme": "classic",
+        "position": "bottom-right"
+    })});
