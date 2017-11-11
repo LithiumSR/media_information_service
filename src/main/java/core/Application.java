d3c6bab39588642a236fe45bafa28f4ee7c27f48
Leonardo Sarra <leonardosarra@outlook.com>
2017-11-11 13:14:10 +0100

    Fix for Heroku deployment

--- src/main/java/core/Application.java
+++ src/main/java/core/Application.java
@@ -7,11 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
-    protected static String config="DEFAULT";
+    protected static String config="HEROKU";
 
     public static void main(String[] args) {
         new MSIConfig("redacted_api.cfg"); //Get Api keys
-        if (config.equals("DEFAULT")) startRabbitMQ();
+        if (config.equals("DEFAULT")||config.equals("HEROKU")) startRabbitMQ();
         SpringApplication.run(Application.class, args); //Start Spring App
         }
 
@@ -20,8 +20,11 @@ public class Application {
     }
     public static void startRabbitMQ(){
         new RabbitSend(); //Setup of the class used to send messages to the receiver
-        Thread t1 = new Thread(new RabbitReceive()); //Start localhost receiver
-        t1.start(); //Start receiver in its own thread
+        if(config.equals("DEFAULT")){
+            Thread t1 = new Thread(new RabbitReceive()); //Start localhost receiver
+            t1.start(); //Start receiver in its own thread
+        }
+
     }
 
 }
--- src/main/java/core/MainController.java
+++ src/main/java/core/MainController.java
@@ -71,7 +71,7 @@ public class MainController {
             e.printStackTrace();
             return String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        if(Application.getConfig().equals("DEFAULT")) RabbitSend.send("Game Request by "+request.getRemoteAddr()+" "+new SimpleDateFormat("yyyy/MM/dd HH:mm:ss")
+        if(Application.getConfig().equals("DEFAULT")||Application.getConfig().equals("HEROKU")) RabbitSend.send("Game Request by "+request.getRemoteAddr()+" "+new SimpleDateFormat("yyyy/MM/dd HH:mm:ss")
                 .format(new Date())+ " : - " +"TITLE:" + media.getTitle()+"\n");
         model.addAttribute("mediaList", a);
         return "result_game";
@@ -95,7 +95,7 @@ public class MainController {
 
         }
         model.addAttribute("mediaList", a);
-        if(Application.getConfig().equals("DEFAULT")) RabbitSend.send("Request by "+request.getRemoteAddr()+" "+new SimpleDateFormat("yyyy/MM/dd HH:mm:ss")
+        if(Application.getConfig().equals("DEFAULT")||Application.getConfig().equals("HEROKU")) RabbitSend.send("Book request by "+request.getRemoteAddr()+" "+new SimpleDateFormat("yyyy/MM/dd HH:mm:ss")
                 .format(new Date())+ " : - " +"TITLE: " + media.getTitle()+ ", ISBN: "+media.getISBN()+"\n");
         return "result_book";
     }
@@ -116,7 +116,7 @@ public class MainController {
             return String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
-        if(Application.getConfig().equals("DEFAULT")) RabbitSend.send("Film Request by "+request.getRemoteAddr()+" "+new SimpleDateFormat("yyyy/MM/dd HH:mm:ss")
+        if(Application.getConfig().equals("DEFAULT")||Application.getConfig().equals("HEROKU")) RabbitSend.send("Film Request by "+request.getRemoteAddr()+" "+new SimpleDateFormat("yyyy/MM/dd HH:mm:ss")
                 .format(new Date())+ " : - " +"TITLE: " + media.getTitle()+"\n");
 
         model.addAttribute("mediaList", a);
@@ -136,7 +136,7 @@ public class MainController {
             e.printStackTrace();
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).toString();
         }
-        if(Application.getConfig().equals("DEFAULT")) RabbitSend.send("Music Request by "+request.getRemoteAddr()+" "+new SimpleDateFormat("yyyy/MM/dd HH:mm:ss")
+        if(Application.getConfig().equals("DEFAULT")||Application.getConfig().equals("HEROKU")) RabbitSend.send("Music Request by "+request.getRemoteAddr()+" "+new SimpleDateFormat("yyyy/MM/dd HH:mm:ss")
                 .format(new Date())+ " : - " +"TITLE: " + media.getTitle()+"\n");
         model.addAttribute("mediaList", a);
         return "result_music";
