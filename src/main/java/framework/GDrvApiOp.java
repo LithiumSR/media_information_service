283bb58ad6640cd40fb2aaa6164985ac09bf5e1e
Leonardo Sarra <leonardosarra@outlook.com>
2017-11-14 20:18:42 +0100

    GDrvApiOp: Improved search method

--- src/main/java/framework/GDrvApiOp.java
+++ src/main/java/framework/GDrvApiOp.java
@@ -12,7 +12,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class GDrvApiOp {
-    //Method used to get all files from a Google Drive folder (subdirectories not included)
+    //Method used to get all files from a Google Drive folder (subdirectories included)
     public static List<String> retrieveAllFiles(String auth, String folder) throws IOException, UnirestException {
         List<String> lis= new LinkedList<String>();
         HttpResponse<JsonNode> jsonResponse = Unirest.get("https://www.googleapis.com/drive/v2/files/root/children?q=title='"+folder+"'").header("Authorization","Bearer "+auth).asJson();
@@ -20,11 +20,11 @@ public class GDrvApiOp {
         JSONArray array = jsonObject.getJSONArray("array");
         for(int i=0;i<array.length();i++){
             JSONArray jarray=array.getJSONObject(i).getJSONArray("items");
-            if(jarray.length()>=1){
+            int j=jarray.length();
+            while(j>0){
                 String id=jarray.getJSONObject(0).getString("id");
-                //System.out.println(id);
                 auxRetrieveAllFiles(lis,auth,"https://www.googleapis.com/drive/v2/files?includeTeamDriveItems=false&pageSize=500&q='"+id+"'%20in%20parents"+"&key="+ MISConfig.getGoogle_api(),id);
-                break;
+                j--;
             }
 
         }
@@ -39,9 +39,15 @@ public class GDrvApiOp {
             JSONArray jarray=array.getJSONObject(j).getJSONArray("items");
             for(int i = 0; i < jarray.length(); i++)
             {
-                String name= jarray.getJSONObject(i).getString("title");
-                lis.add(jarray.getJSONObject(i).getString("title"));
-
+                if(jarray.getJSONObject(i).has("mimeType") && !jarray.getJSONObject(i).get("mimeType").equals("application/vnd.google-apps.folder")){
+                    String name= jarray.getJSONObject(i).getString("title");
+                    lis.add(jarray.getJSONObject(i).getString("title"));
+                }
+                else {
+                    if(jarray.getJSONObject(i).has("id")){
+                        auxRetrieveAllFiles(lis,auth,"https://www.googleapis.com/drive/v2/files?includeTeamDriveItems=false&pageSize=500&q='"+jarray.getJSONObject(i).get("id")+"'%20in%20parents"+"&key="+ MISConfig.getGoogle_api(),parents);
+                    }
+                }
             }
             if(array.getJSONObject(j).has("nextLink")){
                 String next=array.getJSONObject(j).getString("nextLink");
