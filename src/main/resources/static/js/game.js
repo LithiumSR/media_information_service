59aa1341aa98def5d070d0dbc1bd0f30f50f9755
Leonardo Razovic <leorazzo@gmail.com>
2017-12-06 10:09:01 +0100

    Used ESLint to lint JS files

--- src/main/resources/static/js/game.js
+++ src/main/resources/static/js/game.js
@@ -1,67 +1,67 @@
 function initializeGameStorage() {
-    if (typeof(localStorage.games) == "undefined") {
+    if (typeof (localStorage.games) == "undefined") {
         localStorage.games = "[]";
     }
-    if (localStorage.games!="[]") printGameStorage();
-    addEventHandlers()
+    if (localStorage.games != "[]") printGameStorage();
+    addEventHandlers();
 }
 
-    function addGameStorage() {
-        x = document.getElementById("title").value.trim();
-        if (x == "") {
-            alert("Name must be filled out");
-            return false;
-        }
-
-        var currentdate = new Date();
-        var datetime = currentdate.getDate() + "/"
-            + (currentdate.getMonth()+1)  + "/"
-            + currentdate.getFullYear() + " @ "
-            + currentdate.getHours() + ":"
-            + currentdate.getMinutes() + ":"
-            + currentdate.getSeconds();
-
-
-        var u = JSON.parse(localStorage.games);
-        var nextpos = u.length;
-        var o = { title:x,date:datetime};
-
-        u[nextpos] = o;
-        localStorage.games = JSON.stringify(u);
-        return true;
+function addGameStorage() {
+    var x = document.getElementById("title").value.trim();
+    if (x == "") {
+        alert("Name must be filled out");
+        return false;
     }
 
+    var currentdate = new Date();
+    var datetime = currentdate.getDate() + "/" +
+        (currentdate.getMonth() + 1) + "/" +
+        currentdate.getFullYear() + " @ " +
+        currentdate.getHours() + ":" +
+        currentdate.getMinutes() + ":" +
+        currentdate.getSeconds();
 
-    function printGameStorage(){
-        var u = JSON.parse(localStorage.games);
-        var l = u.length;
-        var s = new String("<h3>Search history:</h3>");
-        var i=l-1;
-        while(i>=0){
-            if(i!=l-1) s+="<br>";
 
-            s += "<div class='search'><strong>Titolo: </strong>" + "<span style='display:inline' class='title'>"+u[i].title +"</span>"+"  <strong>Time: </strong>" +u[i].date+"</div>";
-            i--;
+    var u = JSON.parse(localStorage.games);
+    var nextpos = u.length;
+    var o = {
+        title: x,
+        date: datetime
+    };
 
-        }
-        document.getElementById("gameStorage").innerHTML = s;
-        return true;
+    u[nextpos] = o;
+    localStorage.games = JSON.stringify(u);
+    return true;
+}
+
+
+function printGameStorage() {
+    var u = JSON.parse(localStorage.games);
+    var l = u.length;
+    var s = new String("<h3>Search history:</h3>");
+    var i = l - 1;
+    while (i >= 0) {
+        if (i != l - 1) s += "<br>";
+
+        s += "<div class='search'><strong>Titolo: </strong>" + "<span style='display:inline' class='title'>" + u[i].title + "</span>" + "  <strong>Time: </strong>" + u[i].date + "</div>";
+        i--;
+
     }
+    document.getElementById("gameStorage").innerHTML = s;
+    return true;
+}
 
 
 function addEventHandlers() {
     var searchElement = document.getElementsByClassName("search");
-    for (i = 0; i < searchElement.length; i++) {
-        searchElement[i].addEventListener("click",compileForm);
+    for (var i = 0; i < searchElement.length; i++) {
+        searchElement[i].addEventListener("click", compileForm);
     }
 
 }
 
 function compileForm() {
-    if(this.querySelector(".title").innerHTML!="--") {
-        document.getElementById("title").value=this.querySelector(".title").innerHTML;
+    if (this.querySelector(".title").innerHTML != "--") {
+        document.getElementById("title").value = this.querySelector(".title").innerHTML;
     }
 }
-
-
-
