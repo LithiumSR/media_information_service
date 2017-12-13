3a53bcd8b565a97ea76c3d7fd740a97009b3341f
Leonardo Sarra <leonardosarra@outlook.com>
2017-12-13 22:18:37 +0100

    Media results: Updated clean_broken_elements.js to add a generic img if none is found

--- src/main/resources/static/js/clean_broken_elements.js
+++ src/main/resources/static/js/clean_broken_elements.js
@@ -2,7 +2,10 @@ $(document).ready(function () {
     $("img").each(function() {
         var src = $(this).attr("src");
         if(src == 'Information not avaiable') { // or anything else you want to remove...
-            $(this).remove();
+            if ($(this).attr("class")=="music-cover"){
+                $(this).attr("src","./images/no_image_230x230.png")
+            }
+            else $(this).attr("src","./images/no_image_173x260.png");
         }
     });
     $("a").each(function() {
