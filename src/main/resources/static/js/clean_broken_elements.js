7f93372b9e56d6d9044e539657d96e0d1d5d5bec
Leonardo Sarra <leonardosarra@outlook.com>
2017-12-14 23:29:33 +0100

    Media results: Added images if no cover is found

--- src/main/resources/static/js/clean_broken_elements.js
+++ src/main/resources/static/js/clean_broken_elements.js
@@ -3,8 +3,18 @@ $(document).ready(function () {
         var src = $(this).attr("src");
         if (src == 'Information not avaiable') { // or anything else you want to remove...
             if ($(this).attr("class") == "music-cover") {
-                $(this).attr("src", "./images/no_image_230x230.png")
-            } else $(this).attr("src", "./images/no_image_173x260.png");
+                $(this).attr("src", "./images/Music-squashed.png")
+            }
+            else if ($(this).attr("class") == "film-cover"){
+                $(this).attr("src", "./images/Film-squashed.png")
+                $(this).attr("width", "185")
+                $(this).attr("height", "278")
+
+            }
+            else if ($(this).attr("class") == "game-cover"){
+                $(this).attr("src", "./images/Game-squashed.png")
+            }
+            else $(this).attr("src", "./images/Book-squashed.png")
         }
     });
     $("a").each(function () {
