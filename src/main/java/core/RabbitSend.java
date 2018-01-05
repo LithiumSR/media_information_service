28c87b10476c3e1f5920cb40caf346fe8603c885
Leonardo Sarra <leonardosarra@outlook.com>
2018-01-05 17:06:38 +0100

    RabbitMQ: Improved logging

--- src/main/java/core/RabbitSend.java
+++ src/main/java/core/RabbitSend.java
@@ -1,17 +1,18 @@
 package core;
 
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import framework.MISConfig;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 import java.util.concurrent.TimeoutException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 
 public class RabbitSend {
@@ -20,8 +21,10 @@ public class RabbitSend {
     private static boolean durable = false;    //durable - RabbitMQ will never lose the queue if a crash occurs
     private static boolean exclusive = false;  //exclusive - if queue only will be used by one connection
     private static boolean autoDelete = false; //autodelete - queue is deleted when last consumer unsubscribes
-    private static Logger logger = Logger.getLogger("rabbit_send");
+    private static Logger logger;
     public static void init(){
+        LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+        ch.qos.logback.classic.Logger logger = loggerContext.getLogger("core.RabbitSend");
         ConnectionFactory factory = new ConnectionFactory();
         if (!Application.getRabbitStatus().equals("LOCALHOST")) uri = MISConfig.getAMQP();
         if (uri == null) uri = "amqp://guest:guest@localhost";
@@ -37,19 +40,18 @@ public class RabbitSend {
             e.printStackTrace();
         }
         Connection connection = null;
-        //System.out.println(uri);
         try {
             connection = factory.newConnection();
             channel = connection.createChannel();
-            logger.log(Level.INFO,"Connection enstablished with rabbitmq server");
+            logger.info("Connection enstablished with rabbitmq server");
         } catch (IOException e) {
             e.printStackTrace();
             Application.setRabbitStatus("NORABBIT");
-            logger.log(Level.SEVERE, "RabbitMQ got disabled because of unintended behavior");
+            logger.warn("RabbitMQ got disabled because of unintended behavior");
         } catch (TimeoutException e) {
             e.printStackTrace();
             Application.setRabbitStatus("NORABBIT");
-            logger.log(Level.SEVERE, "RabbitMQ got disabled because of unintended behavior");
+            logger.warn("RabbitMQ got disabled because of unintended behavior");
         }
     }
     public static void send(String toSend,String queue){
@@ -60,12 +62,11 @@ public class RabbitSend {
             //System.out.println(" [x] Sent '" + message + "'");
         } catch (IOException e) {
             e.printStackTrace();
-
         }
         catch (NullPointerException e) {
             e.printStackTrace();
             Application.setRabbitStatus("NORABBIT");
-            logger.log(Level.SEVERE, "RabbitMQ got disabled because of unintended behavior");
+            logger.warn("RabbitMQ got disabled because of unintended behavior");
         }
 
     }
